53dc6b39e36ca0b549cd29c0ebc9e875942bae7b
Petr Brozek <petr.brozek@fischer.cz>
2024-10-29 10:45:55 +0100

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Button label="Button" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+<script setup lang="ts">
+import Button from 'primevue/button'
 </script>
 
 <style>
@@ -21,7 +12,6 @@ export default defineComponent({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
